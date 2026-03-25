5959b9e862c19255eb5a99a8598ac80dedc0d9f4
Nadav <nadavlac@wix.com>
2026-03-25 16:32:11 +0100

    feat: add library entry point, export defineEval from package

---
+++ src/core/index.ts
@@ -0,0 +1,20 @@
+export { defineEval } from './define-eval';
+export { loadEvalConfig, resolveTask } from './config';
+export type {
+    DefineEvalInput,
+    DefineEvalTaskInput,
+    DefineEvalGraderInput,
+    DefineEvalConversationInput,
+    EvalConfig,
+    EvalDefaults,
+    EvalTaskConfig,
+    EvalGraderConfig,
+    ResolvedTask,
+    ResolvedGrader,
+    WorkspaceMapping,
+    EnvironmentConfig,
+    ConversationConfig,
+    ConversationReplyConfig,
+    ConversationPersonaConfig,
+    ConversationCompletionConfig,
+} from './config.types';
